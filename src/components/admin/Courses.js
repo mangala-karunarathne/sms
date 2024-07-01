e902d0c23c4038cf2270eb79914fc3dd48f95809
mangala-karunarathne <mangalaicc@gmail.comm>
2024-07-02 00:03:24 +0530

    CHG | Add Tables

--- src/components/admin/Courses.js
+++ src/components/admin/Courses.js
@@ -1,13 +1,21 @@
 import React from "react";
 import AdminNavbar from "./AdminNavbar";
+import Table from "./Table";
 
 export default function Courses() {
+  const courses = [
+    { name: 'Intro to JavaScript', regNumber: 'Course001' },
+    { name: 'Advanced React', regNumber: 'Course002' },
+  ];
   return (
     <div className="flex h-screen">
       <AdminNavbar />
       <div className="flex-1 p-4">
         <div>
           <h1>Courses</h1>
+          <div>
+            <Table data={courses}/>
+          </div>
         </div>
       </div>
     </div>
--- src/components/admin/Students.js
+++ src/components/admin/Students.js
@@ -1,13 +1,21 @@
 import React from "react";
 import AdminNavbar from "./AdminNavbar";
+import Table from "./Table";
 
 export default function Students() {
+  const students = [
+    { name: "John Doe", regNumber: "Student001" },
+    { name: "Jane Smith", regNumber: "Student002" },
+  ];
   return (
     <div className="flex h-screen">
       <AdminNavbar />
       <div className="flex-1 p-4">
         <div>
           <h1>Students</h1>
+          <div>
+            <Table data={students} />
+          </div>
         </div>
       </div>
     </div>
